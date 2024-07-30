b82f6ceb566b9dfe1ea8a6b481e198d0609bab32
unknown <shabnam.sadikhzada@gmail.com>
2024-07-30 23:15:59 +0400

    template added

---
+++ TemplatePractice/Models/Categories.cs
@@ -0,0 +1,8 @@
+public class Categories
+            {
+             public int CategoryID { get; set; }
+ public string CategoryName { get; set; }
+ public string Description { get; set; }
+ public byte[] Picture { get; set; }
+
+            }
